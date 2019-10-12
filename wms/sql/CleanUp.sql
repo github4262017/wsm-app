ae7f435059b8e8dcef8fe924f4dbad3b56420c1f
harik <harik@DESKTOP-8QNUC8S>
2019-10-12 14:14:39 +0530

    Updated DB and Cleanup sql and SeatDetails csv files

---
+++ wms/sql/CleanUp.sql
@@ -0,0 +1,20 @@
+truncate table wms_email_jobs;
+truncate table wms_fa_requests;
+truncate table wms_floor_details_new;
+truncate table wms_history;
+truncate table wms_pm_details;
+truncate table wms_pm_requests;
+truncate table wms_project_details;
+truncate table wms_allocation_seats;
+truncate table wms_employee_seats_asign;
+truncate wms_workstation_status;//Confirm Once
+truncate wms_coordinates;//Confirm Once
+
+UPDATE wms_request_id SET request_id=0;
+
+
+
+
+
+
+
