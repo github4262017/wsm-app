732250e1e3698d49899692cc82f2f5e086247832
thiru <thiru@ation.co>
2020-10-09 13:47:35 +0530

    Database Cleanup and Bulk upload5-20-2020

--- wms/sql/CleanUp.sql
+++ wms/sql/CleanUp.sql
@@ -1,3 +1,4 @@
+// Start Cleanup new request 
 truncate table wms_email_jobs;
 truncate table wms_fa_requests;
 truncate table wms_floor_details_new;
@@ -6,16 +7,24 @@ truncate table wms_pm_requests;
 truncate table wms_project_details;
 truncate table wms_allocation_seats;
 truncate table wms_employee_seats_asign;
+truncate table wms_bulkupload_jobs;
+
+UPDATE wms_request_id SET request_id=0;
+UPDATE wms_workstation_status SET current_status=0;
+update `wms_workstation_status` set `request_id`='';
+update `wms_workstation_status` set `project_id`='';
+update wms_workstation_status set employees='';
+// End Cleanup new request
+
+//Workstation status details
 truncate wms_workstation_status;
 truncate wms_coordinates;
 
-UPDATE wms_request_id SET request_id=0;
 
-
-INSERT INTO wms_workstation_status  (floor_id, workstation_no)
+INSERT INTO wms_workstation_status (floor_id, workstation_no)
 SELECT floor_id, workstation_no FROM wms_coordinates;
 
-
+// To Update SonyEmployeeDetails with  division
 UPDATE `wms_sony_emp_details` SET division = 'Infosec'  WHERE `division` = 'Information Security'
 UPDATE `wms_sony_emp_details` SET division = 'SARD'  WHERE `division` = 'Software Architecture Division'
 UPDATE `wms_sony_emp_details` SET division = 'ISBL'  WHERE `division` = 'Information System Bangalore'
